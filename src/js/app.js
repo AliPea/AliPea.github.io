aaabbe868c273d3d618871b967dd6177403f218f
AliPea <web.alipea@gmail.com>
2017-09-22 14:57:32 -0700

    uploade new content

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sweetScroll = new SweetScroll({/* some options */});
 
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+   particlesJS.load(@dom-id, @path-json, @callback (optional)); 
   particlesJS('particles-js', {
     "particles": {
       "number": {
